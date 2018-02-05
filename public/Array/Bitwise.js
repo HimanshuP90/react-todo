a97798164f56c27fb31dbd11e44ad739b594d699
HimanshuP90 <himanshup895@gmail.com>
2018-02-05 10:06:08 +0530

    Concepts added

--- public/Array/Bitwise.js
+++ public/Array/Bitwise.js
@@ -15,6 +15,18 @@ Interesting Facts about Bitwise Operators:
 
 ~(bitwise NOT): Takes one number and inverts all bits of it.
 
+The left-shift and right-shift operators are equivalent to multiplication and division by 2 respectively.
+The result of logical operators (&&, || and !) is either 0 or 1, but bitwise operators return an integer value.
+
+int main()
+{
+   int x = 2, y = 5;
+   (x & y)? printf("True ") : printf("False ");
+   (x && y)? printf("True ") : printf("False ");
+   return 0;
+}
+// Output: False True
+
 */
 
 let a = 5, b = 9;
@@ -24,3 +36,27 @@ console.log(a | b);
 console.log(a ^ b);
 console.log(a << b);
 console.log(a >> b);
+
+
+//Getting lowest set bit of a number:
+
+num = 7;
+num & ~(num -1); 
+
+
+//Count set bits in an integer
+num &= (num-1);
+
+//Count total bits in a number
+
+num = 18;
+
+	while (n)
+	   {
+	        count++;
+	        n >>= 1;
+	    }
+	return count;
+
+//Position of rightmost set bit
+ n &  ~(n-1)
